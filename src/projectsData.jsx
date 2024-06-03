80434e826425b5553e6f7c0ec16e0f8559f5ef85
Ahmed <ahmedkhan895.ak@gmail.com>
2024-06-03 21:39:48 +0100

    fixed project links

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -4,8 +4,8 @@ import battleshipImage from './assets/battleship.png';
 const projectsArray = [
   {
     name: 'Battleship',
-    codeLink: 'google.com',
-    liveLink: 'google.com',
+    codeLink: 'https://github.com/AdotK8/battleship',
+    liveLink: 'https://adotk8.github.io/battleship/',
     image: battleshipImage,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
