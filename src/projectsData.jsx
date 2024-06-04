6c4f72b36a64fd2f056fa787b8b4b59a10aa0a36
Ahmed <ahmedkhan895.ak@gmail.com>
2024-06-05 00:30:09 +0100

    changed styling colours and added cv application project

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -1,4 +1,4 @@
-import placeholderImage from './assets/placeholder.jpg';
+import cvApplicationImage from './assets/cv-application.png';
 import battleshipImage from './assets/battleship.png';
 import memoryCardImage from './assets/memory-card.png';
 import yasePropertyImage from './assets/yase-property.png';
@@ -14,9 +14,18 @@ const projectsArray = [
     subheader: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
   },
   {
+    name: 'CV Application',
+    codeLink: 'https://github.com/AdotK8/cv-application',
+    liveLink: 'https://cv-application-c680e5d1f0ee.herokuapp.com/',
+    image: cvApplicationImage,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    subheader: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+  },
+  {
     name: 'Memory Card Game',
-    codeLink: 'https://memory-card-3716161eff09.herokuapp.com/',
-    liveLink: 'https://github.com/AdotK8/memory-card',
+    codeLink: 'https://github.com/AdotK8/memory-card',
+    liveLink: 'https://memory-card-3716161eff09.herokuapp.com/',
     image: memoryCardImage,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
