1fe2f03c36a92df24ec7e2fc36553049fe3a11a9
Ahmed <ahmedkhan895.ak@gmail.com>
2024-06-04 14:11:17 +0100

    finished styling for projects section, need to add contact section

--- src/projectsData.jsx
+++ src/projectsData.jsx
@@ -1,21 +1,32 @@
 import placeholderImage from './assets/placeholder.jpg';
 import battleshipImage from './assets/battleship.png';
+import memoryCardImage from './assets/memory-card.png';
+import yasePropertyImage from './assets/yase-property.png';
 
 const projectsArray = [
   {
-    name: 'Battleship',
-    codeLink: 'https://github.com/AdotK8/battleship',
-    liveLink: 'https://adotk8.github.io/battleship/',
-    image: battleshipImage,
+    name: 'Yase Property Valuation Widget',
+    codeLink: 'google.com',
+    liveLink: 'google.com',
+    image: yasePropertyImage,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     subheader: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
   },
   {
-    name: 'test',
-    codeLink: 'google.com',
-    liveLink: 'google.com',
-    image: placeholderImage,
+    name: 'Memory Card Game',
+    codeLink: 'https://memory-card-3716161eff09.herokuapp.com/',
+    liveLink: 'https://github.com/AdotK8/memory-card',
+    image: memoryCardImage,
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    subheader: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
+  },
+  {
+    name: 'Battleship Game',
+    codeLink: 'https://github.com/AdotK8/battleship',
+    liveLink: 'https://adotk8.github.io/battleship/',
+    image: battleshipImage,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
     subheader: 'Lorem ipsum dolor sit amet, consectetur adipiscing',
