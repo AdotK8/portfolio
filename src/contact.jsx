c6715fce05c342a66d0533cf4ce8b740a65d24b0
Ahmed <ahmedkhan895.ak@gmail.com>
2024-10-14 21:23:00 +0100

    updated cv link

--- src/contact.jsx
+++ src/contact.jsx
@@ -24,7 +24,7 @@ function Contact() {
         </a>
 
         <a
-          href="https://drive.google.com/file/d/1WtWk-LOKgvoR1LPVAjaL2wBIlTwJpldz/view?usp=sharing"
+          href="https://drive.google.com/file/d/1k0A4avIESuFAApZsowSlxTUKa5jg7vq7/view?usp=sharing"
           target="_blank"
           rel="noopener noreferrer"
         >
